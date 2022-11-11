3f70dc8e139f3fb171d297b11df8cd0f778db43d
sushantshirsekar <sushantshirsekar60@gmail.com>
2022-11-11 14:20:53 +0530

    assignment12

--- main.js
+++ main.js
@@ -8,6 +8,26 @@ const formInput = document.querySelector('#my_form');
         const msg = document.querySelector('.msg');
         const afterSubmit = document.querySelector('.submitted');
         
+        let dataAvail = document.getElementById('items');
+
+        dataAvail.addEventListener('click' , deleteData);
+
+        function deleteData(e)
+        {
+            if(e.target.classList.contains('btn'))
+            {
+                confirm('Are you sure?')
+                {
+                    let li = e.target.parentElement;
+                    dataAvail.removeChild(li);
+                }
+            }
+        }
+        
+        
+        
+
+        
 
         formInput.addEventListener('submit', addValues)
         let details = [];
@@ -32,7 +52,7 @@ const formInput = document.querySelector('#my_form');
             
 
             localStorage.setItem('user', JSON.stringify(details));
-            
+            localStorage.removeItem(mail);
 
             afterSubmit.innerHTML = 'Success!!';
             setTimeout(() => afterSubmit.remove(), 3000);
@@ -41,7 +61,28 @@ const formInput = document.querySelector('#my_form');
             mailInput.value = '';
             phoneInput.value = '';
             timeInput.value = '';
+            var data = document.createElement('li');
+
+            let details1 = JSON.stringify(myObj);
+            
+
+            data.appendChild(document.createTextNode(details1));
+            data.style.fontSize = '20px';
+            let libtn = document.createElement('button');
+            let txtbtn = document.createTextNode('delete');
+            libtn.appendChild(txtbtn);
+            libtn.className = 'btn';
+            data.appendChild(libtn);
+            var insert4 = document.querySelector('div .data-header');
+
+            var insert3 = document.querySelector('h2 .insertbeforespan');
+            dataAvail.insertBefore(data, insert3);
+            
+            
 
         }
+
+        
+
 
         
